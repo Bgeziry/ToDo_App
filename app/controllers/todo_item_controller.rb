4049585128255b4c1fded2bbc93cb561279f1e70
Bgeziry <b.geziry@robone.net>
2017-05-16 07:24:30 +0200

    Send a json request

--- app/controllers/todo_item_controller.rb
+++ app/controllers/todo_item_controller.rb
@@ -1,22 +1,29 @@
 class TodoItemController < ApplicationController
+  respond_to :json
+
   def index
     @todo_item = TodoItem.all
+    respond_with @todo_item
   end
 
   def show
     @todo_item = TodoItem.find(params[:id])
+    respond_with @todo_item
   end
 
   def new
     @todo_item = TodoItem.new
+    respond_with @todo_item
   end
 
   def edit
     @todo_item = TodoItem.find(params[:id])
+    respond_with @todo_item
   end
 
   def create
     @todo_item = TodoItem.create(todo_item_params)
+    respond_with @todo_item
   end
 
   def update
@@ -33,6 +40,7 @@ class TodoItemController < ApplicationController
       @todo_item = TodoItem.destroy
 
       redirect_to todo_items_path
+      respond_with @todo_item
   end
 
 
